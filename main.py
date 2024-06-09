84d383be4292beebaad6efad7e65e7d22d7f56ce
KarthikGowdaRamakrishna <144963620+KarthikGowdaRamakrishna@users.noreply.github.com>
2024-06-09 19:14:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,23 @@ import os
 
 load_dotenv()
 
+st.write("GROQ_API_KEY:", st.secrets["GROQ_API_KEY"])
+st.write(
+    "Has environment variables been set:",
+    os.environ["GROQ_API_KEY"] == st.secrets["GROQ_API_KEY"],
+)
+
+groq_api_key = os.environ['GROQ_API_KEY']
+
 # Try to get the GROQ_API_KEY from environment variables or Streamlit secrets
-try:
-    groq_api_key = os.environ['GROQ_API_KEY']
-except KeyError:
-    try:
-        groq_api_key = st.secrets['GROQ_API_KEY']
-    except KeyError:
-        st.error("GROQ_API_KEY not found. Please set it in your environment variables or Streamlit secrets.")
-        st.stop()
+# try:
+#     groq_api_key = os.environ['GROQ_API_KEY']
+# except KeyError:
+#     try:
+#         groq_api_key = st.secrets['GROQ_API_KEY']
+#     except KeyError:
+#         st.error("GROQ_API_KEY not found. Please set it in your environment variables or Streamlit secrets.")
+#         st.stop()
 
 loader = CSVLoader(file_path="NewMe.csv")
 documents = loader.load()
